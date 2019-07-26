23a2cf0cfaca5bbf14978b3210ff2e21ca67f51e
Arun Bhattasali <abhattasali@gmail.com>
2019-07-26 16:05:08 -0700

    snapstudy arunstestbranch

---
+++ SnapStudy/UICardTest.swift
@@ -0,0 +1,28 @@
+//
+//  UICardTest.swift
+//  SnapStudy
+//
+//  Created by Arun Bhattasali on 7/26/19.
+//  Copyright © 2019 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class UICardTest: UIViewController {
+    
+    var card_flashset: FlashSet!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("test on uiCardVC: \(card_flashset.getSetName())")
+        /*for w in card_flashset.myWordCards.keys {
+            print("\(w)")
+            print("\((card_flashset.myWordCards[w]?.definition)!)\n\n")
+        }*/
+
+      
+    }
+    
+
+}
